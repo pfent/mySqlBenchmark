3294d855f20a6eaab3e3459a6fa25c5d18d43cef
pfent <philipp@fent.de>
2018-07-24 13:07:31 +0200

    do benchmark and add TODO to query connection type

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+#include <chrono>
 #include <iostream>
 #include <mysqlx/xdevapi.h>
 
+template<typename T>
+auto bench(T &&fun) {
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    fun();
+
+    const auto end = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration<double>(end - start).count();
+}
+
+/**
+ * See:
+ * https://dev.mysql.com/doc/dev/connector-cpp/8.0/devapi_ref.html
+ * https://dev.mysql.com/doc/dev/connector-cpp/8.0/group__devapi.html
+ */
 int main(int argc, const char *argv[]) {
     const auto url = [&] {
         if (argc < 2) {
@@ -9,20 +26,33 @@ int main(int argc, const char *argv[]) {
         }
         return argv[1];
     }();
-
+    
     auto session = mysqlx::Session(url);
+    /*
     auto schema = session.getDefaultSchema();
     auto tables = schema.getTableNames();
 
     for (const auto &table : tables) {
         std::cout << table << '\n';
     }
+     */
+
+    // TODO: query connection type:
+    // select connection_type from performance_schema.threads where connection_type is not null and processlist_state is not null;
 
     auto statement = session.sql("SELECT 1;");
-    auto result = statement.execute();
-    auto row = result.fetchOne();
-    std::cout << row[0] << '\n';
-    assert(row[0].get<int>() == 1);
+    auto iterations = size_t(1e6);
+    auto timeTaken = bench([&] {
+        for (size_t i = 0; i < iterations; ++i) {
+            auto result = statement.execute();
+            auto row = result.fetchOne();
+            if (row[0].get<int>() != 1) {
+                throw std::runtime_error{"unexpected return value"};
+            }
+        }
+    });
+
+    std::cout << iterations / timeTaken << "msg/s\n";
 
     return 0;
 }
